c5736b4d98f203ce077e1ac55d5e4e96567089c6
Anurag <anuragmishra11904@gmail.com>
2025-11-22 02:47:17 +0530

    Module.css

---
+++ 05-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Component/button/Button.jsx'
+import Header from './Component/header/Header.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ 05-CSS/src/Component/button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import style from './Button.module.css'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>This is button</div>
+  )
+}
+
+export default Button
